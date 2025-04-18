a79f96bf23461a3a5d56ed0c456bc1494299f895
aliciayuting <yy354@cornell.edu>
2025-04-18 14:01:17 -0400

    added plot colors soft link

---
+++ tp_summary/ppl2_summary/plot_colors.py
@@ -0,0 +1,10 @@
+# frameworks = ['Torch Serve', 'Ray Monolithic', 'Ray Microservice', 'Vortex Monolithic', 'Vortex Microservice']
+# colors = ['#444444', '#E57373', '#A61B1B', '#5B8DB8', '#264E86']
+
+frame_colors = {'Torch Serve': '#444444',
+                'Ray Monolithic': '#E57373',  #blue
+                'Ray Microservice': '#264E86',
+                'Vortex Monolithic': '#5B8DB8',  #red
+                'Vortex Microservice': '#A61B1B',
+                'Vortex Microservice (TCP)': '#FF7A29' }
+
